9c347b8238f7bb45aa29ca5ce7e3e675740a16f1
trpoliSo <tryznapalina@gmail.com>
2025-12-11 23:54:57 +0300

    alteration

--- src/main/java/com/auditJpa/JpaAudit.java
+++ src/main/java/com/auditJpa/JpaAudit.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityListeners;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
